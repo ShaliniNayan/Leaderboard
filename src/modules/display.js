559c583a060ee347fd1784101c9d11ada356c0b6
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-05-17 21:36:20 +0530

    Add modules folder and update index.js

---
+++ src/modules/display.js
@@ -0,0 +1,44 @@
+import { getData, postData } from './add.js';
+
+function Score(score, name) {
+  this.score = score;
+  this.user = name;
+}
+
+export default class gamesScores {
+  constructor() {
+    this.scores = [];
+  }
+
+  addScore = (score, name) => {
+    const newScore = new Score(score, name);
+    postData(newScore);
+  };
+
+  getScores = async () => {
+    const scores = await getData();
+
+    this.scores = scores;
+  };
+
+  displayScores = () => {
+    const container = document.querySelector('.scoresList');
+    container.innerHTML = '';
+    this.scores.forEach((score) => {
+        const scoreEl = document.createElement('li');
+        scoreEl.classList.add('score');
+    
+        const nameEl = document.createElement('span');
+        nameEl.classList.add('name');
+        nameEl.innerText = score.user;
+        scoreEl.appendChild(nameEl);
+    
+        const valueEl = document.createElement('span');
+        valueEl.classList.add('score');
+        valueEl.innerText = score.score;
+        scoreEl.appendChild(valueEl);
+    
+        container.appendChild(scoreEl);
+    });
+  };
+}
